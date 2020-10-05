850ba2c67213a4fe19c4e453aa7c2b8adbed8e6a
Rafael Daily <38022239+rafaeldaily@users.noreply.github.com>
2020-10-04 21:48:58 -0300

    Update Main.java

--- BuscaEmProfundidade/src/main/Main.java
+++ BuscaEmProfundidade/src/main/Main.java
@@ -3,10 +3,14 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		Grafo grafo = new Grafo(7);
+				
+		Grafo grafo = new Grafo(5);
 		Busca busca = new Busca(grafo);
-		busca.buscaProfundidade();
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o nó inicial");
+		n = s.nextInt();
+		busca.buscaProfundidade(n);
 
 	}
 }
